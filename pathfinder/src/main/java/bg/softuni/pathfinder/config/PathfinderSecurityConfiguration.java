9703e19ef82cd0cd10030d2dfea9080b9da900ba
mporyazova <mporyazova@yahoo.com>
2022-08-28 22:07:02 +0300

    Spring Security Config demo.

--- pathfinder/src/main/java/bg/softuni/pathfinder/config/PathfinderSecurityConfiguration.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/config/PathfinderSecurityConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 // 1. add spring security starter dependency
 // 2. Define the following beans
-//    a. SecurityFilterChain -> here we describe authnetication rules, how to perform login/logout etc.
+//    a. SecurityFilterChain -> here we describe authentication rules, how to perform login/logout etc.
 //    b. PasswordEncoder -> used when saving a User in the DB to hash the password,
 //                          spring security uses it to validate password when logging
 //    c. UserDetailsService -> shows Spring how to fetch the user from the db, wraps the User entity into a UserDetails interface impl
